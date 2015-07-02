c93b32c204688e5d653dce8c2f734ac94e2c2e81
Roger Song <Roger.Song.NY@gmail.com>
2015-07-02 11:37:30 -0400

    Removed unnecessary labels
    Custom post type label boilerplate had way more options than it actually
    needed. Shortened it down to just what was necessary.

--- word-of-the-day/word-of-the-day.php
+++ word-of-the-day/word-of-the-day.php
@@ -34,25 +34,13 @@ if ( ! defined( 'WPINC' ) ) {
 /********** Register Word of the Day custom post type **********/
 function wotd_custom_post_type() {
 	$labels = array(
-		'name'                => _x( 'Word of the Day', 'Post Type General Name', 'text_domain' ),
-		'singular_name'       => _x( 'Word of the Day', 'Post Type Singular Name', 'text_domain' ),
-		'menu_name'           => __( 'Word of the Day', 'text_domain' ),
-		'name_admin_bar'      => __( 'Word of the Day', 'text_domain' ),
-		'parent_item_colon'   => __( 'Parent Item:', 'text_domain' ),
-		'all_items'           => __( 'All Items', 'text_domain' ),
-		'add_new_item'        => __( 'Add New Item', 'text_domain' ),
-		'add_new'             => __( 'Add New', 'text_domain' ),
-		'new_item'            => __( 'New Item', 'text_domain' ),
-		'edit_item'           => __( 'Edit Item', 'text_domain' ),
-		'update_item'         => __( 'Update Item', 'text_domain' ),
-		'view_item'           => __( 'View Item', 'text_domain' ),
-		'search_items'        => __( 'Search Item', 'text_domain' ),
-		'not_found'           => __( 'Not found', 'text_domain' ),
-		'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+		'name'                => _x( 'Word of the Day', 'Post Type General Name' ),
+		'singular_name'       => _x( 'Word of the Day', 'Post Type Singular Name' ),
+		'menu_name'           => __( 'Word of the Day' ),
 	);
 	$args = array(
 		'label'               => __( 'wotd', 'text_domain' ),
-		'description'         => __( 'Word', 'text_domain' ),
+		'description'         => __( 'Word of the day', 'text_domain' ),
 		'labels'              => $labels,
 		'supports'            => array( 'title', 'editor', 'thumbnail' ),
 		'taxonomies'          => array( 'category', 'post_tag' ),
